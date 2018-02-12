3c65d8e94430e574902361b9ebff17f8e64b03a4
Antal Áron <antalaron@antalaron.hu>
2018-02-12 19:59:05 +0100

    Allow Symfony 4.0

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -13,3 +13,13 @@ wget http://getcomposer.org/composer.phar
 php composer.phar install --dev
 ');
 }
+
+if (!class_exists('PHPUnit\Framework\Assert')) {
+    class_alias('PHPUnit\Framework\Assert', 'PHPUnit\Framework\Assert');
+}
+
+if (class_exists('Symfony\Component\Validator\Test\ConstraintValidatorTestCase')) {
+    class_alias('Symfony\Component\Validator\Test\ConstraintValidatorTestCase', 'Antalaron\Component\VatNumberValidator\Tests\AbstractConstraintValidatorTest');
+} else {
+    class_alias('Symfony\Component\Validator\Tests\Constraints\AbstractConstraintValidatorTest', 'Antalaron\Component\VatNumberValidator\Tests\AbstractConstraintValidatorTest');
+}
